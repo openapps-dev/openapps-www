e67a87a51f14069290647997d6d6f1e272bae2ab
Iqro-dev <igor.sokolowski.code@gmail.com>
2023-09-29 14:44:16 +0200

    feat(pages/app): fill in the metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../layouts/default";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Openapps Website</title>
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
