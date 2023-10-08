53a661a3bcf1ff9c555aa25682f3ea9e6c0f2d5c
Iqro-dev <igor.sokolowski.code@gmail.com>
2023-10-08 20:04:39 +0200

    feat(public): add banner and logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,19 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Openapps Website</title>
+
+        <meta charSet="utf-8" />
+
+        <meta name="description" content="Openapps Portfolio Website" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link
+          rel="icon"
+          href="./openapps_logo.svg"
+          type="image/svg+xml"
+          sizes="any"
+        />
       </Head>
 
       <Layout>
